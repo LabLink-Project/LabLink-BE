575357400f96be45e97c1e3132ad15eff1aed0af
rhdqors <rhdqors@gmail.com>
2023-04-04 01:17:11 +0900

    feat: 삭제 파일 복구
    복구 완료

--- src/main/java/com/example/hhproject0/user/repository/TermsRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.lablink.user.repository;
-
-import com.example.lablink.user.entity.Terms;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TermsRepository extends JpaRepository<Terms, Long> {
-}
