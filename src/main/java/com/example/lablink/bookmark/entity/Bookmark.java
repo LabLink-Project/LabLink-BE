1376e2d9e5f22eb3764c76d4dce2a432e36e337e
SoMin <thals0107@naver.com>
2023-04-06 11:56:29 +0900

    feat: Create bookmark feature

---
+++ src/main/java/com/example/lablink/bookmark/entity/Bookmark.java
@@ -0,0 +1,26 @@
+package com.example.lablink.bookmark.entity;
+
+import com.example.lablink.user.entity.User;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+public class Bookmark {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Long studyId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    public Bookmark(Long studyId, User user) {
+        this.studyId = studyId;
+        this.user = user;
+    }
+}
