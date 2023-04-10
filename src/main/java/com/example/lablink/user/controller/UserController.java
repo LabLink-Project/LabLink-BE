4bdb5ecfe11e2635755995cd333af10005803a03
rhdqors <rhdqors@gmail.com>
2023-04-10 18:28:36 +0900

    feat: user 로그아웃 추가
    UserController 로그아웃 기능 추가 (config에 추가해놔서 Controller에 필요없을수도 -> 프론트와 같이 테스트 진행해야할듯)

--- src/main/java/com/example/lablink/user/controller/UserController.java
+++ src/main/java/com/example/lablink/user/controller/UserController.java
@@ -4,13 +4,17 @@ import com.example.lablink.message.ResponseMessage;
 import com.example.lablink.user.dto.request.LoginRequestDto;
 import com.example.lablink.user.dto.request.SignupRequestDto;
 import com.example.lablink.user.dto.request.UserEmailCheckRequestDto;
+import com.example.lablink.user.security.UserDetailsImpl;
 import com.example.lablink.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
@@ -30,14 +34,25 @@ public class UserController {
     // 유저 로그인
     @PostMapping("/users/login")
     public ResponseEntity login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
-        userService.login(loginRequestDto, response);
-        return ResponseMessage.SuccessResponse("로그인 완료", "");
+        return ResponseMessage.SuccessResponse(userService.login(loginRequestDto, response), "");
     }
 
-    // 유저 이메일 재입력 체크
+    // 유저 이메일 중복 체크
     @PostMapping("/users/signup/email-check")
     public ResponseEntity emailCheck(@RequestBody @Valid UserEmailCheckRequestDto userEmailCheckRequestDto) {
-        userService.emailCheck(userEmailCheckRequestDto);
-        return ResponseMessage.SuccessResponse("사용 가능합니다.", "");
+        return ResponseMessage.SuccessResponse(userService.emailCheck(userEmailCheckRequestDto), "");
+    }
+
+    // 유저 로그아웃
+    //csrf 방지위해 post방식
+    @PostMapping("/users/logout")
+    public void logout(HttpServletRequest request, HttpServletResponse response) {
+        new SecurityContextLogoutHandler().logout(request, response, null);
+    }
+
+    // 유저 회원탈퇴
+    @PostMapping("/users/mypages/delete")
+    public ResponseEntity deleteUser (@AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletResponse response) {
+        return ResponseMessage.SuccessResponse(userService.deleteUser(userDetails, response), "");
     }
 }
