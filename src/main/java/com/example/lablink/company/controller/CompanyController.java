ce5a45fe664d3413044180428d2b288393c85a43
rhdqors <rhdqors@gmail.com>
2023-04-12 22:48:57 +0900

    feat: 기업 회원 탈퇴 추가
    기업 회원 탈퇴기능 구현. StudyService & StudyRepository 부분 탈퇴위한 메서드 추가

--- src/main/java/com/example/lablink/company/controller/CompanyController.java
+++ src/main/java/com/example/lablink/company/controller/CompanyController.java
@@ -4,10 +4,15 @@ import com.example.lablink.company.dto.request.CompanyEmailCheckRequestDto;
 import com.example.lablink.company.dto.request.CompanyLoginRequestDto;
 import com.example.lablink.company.dto.request.CompanyNameCheckRequestDto;
 import com.example.lablink.company.dto.request.CompanySignupRequestDto;
+import com.example.lablink.company.security.CompanyDetailsImpl;
 import com.example.lablink.company.service.CompanyService;
 import com.example.lablink.message.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,36 +20,44 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+@Tag(name = "Company", description = "Company API")
 @RestController
 @RequiredArgsConstructor
 public class CompanyController {
 
     private final CompanyService companyService;
 
-    // 기업 회원가입
+    @Operation(summary = "기업 회원가입", description = "기업 회원가입")
     @PostMapping("/companies/signup")
     public ResponseEntity companySignup(@RequestBody CompanySignupRequestDto companySignupRequestDto) {
         return ResponseMessage.SuccessResponse(companyService.companySignup(companySignupRequestDto), "");
     }
 
-    // 기업 로그인
+    @Operation(summary = "기업 로그인", description = "기업 로그인")
     @PostMapping("/companies/login")
     public ResponseEntity companyLogin(@RequestBody CompanyLoginRequestDto companyLoginRequestDto, HttpServletResponse response) {
         companyService.companyLogin(companyLoginRequestDto, response);
         return ResponseMessage.SuccessResponse("로그인 완료", "");
     }
 
-    // 기업 이메일 재입력 체크
+    @Operation(summary = "기업 이메일 체크", description = "기업 이메일 체크")
     @PostMapping("/companies/signup/email-check")
     public ResponseEntity emailCheck(@RequestBody @Valid CompanyEmailCheckRequestDto companyEmailCheckRequestDto) {
         companyService.emailCheck(companyEmailCheckRequestDto);
         return ResponseMessage.SuccessResponse("사용 가능합니다.", "");
     }
 
-    // 기업명 재입력 체크
+    @Operation(summary = "기업명 체크", description = "기업명 체크")
     @PostMapping("/companies/signup/company-name-check")
     public ResponseEntity companyNameCheck(@RequestBody @Valid CompanyNameCheckRequestDto companyNameCheckRequestDto) {
         companyService.companyNameCheck(companyNameCheckRequestDto);
         return ResponseMessage.SuccessResponse("사용 가능합니다.", "");
+    }
+
+    @Operation(summary = "기업 회원 탈퇴", description = "기업 회원 탈퇴")
+    @DeleteMapping("/companies")
+    public ResponseEntity deleteCompany(@AuthenticationPrincipal CompanyDetailsImpl companyDetails, HttpServletResponse response) {
+        companyService.deleteCompany(companyDetails, response);
+        return ResponseMessage.SuccessResponse("탈퇴 완료.", "");
     }
 }
