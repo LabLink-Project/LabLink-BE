2f4d3666b778c0fc7ecbeccc1eeb1802f43b5e4b
rhdqors <rhdqors@gmail.com>
2023-04-14 22:49:48 +0900

    feat: 마이페이지 정보 수정 완료
    1. 내 정보 수정을 위해 로그인 유저의 비밀번호 확인. -> 2번의 이름, 생년월일에 들어갈 값 반환.
    2. 이름 및 기타 정보 수정.
    3. 비밀번호 수정

---
+++ src/main/java/com/example/lablink/user/dto/request/MyPageCheckRequestDto.java
@@ -0,0 +1,45 @@
+package com.example.lablink.user.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import java.util.Date;
+
+@Getter
+@AllArgsConstructor
+public class MyPageCheckRequestDto {
+    @NotNull(message = "정보를 입력해 주세요")
+    @NotBlank(message = "정보를 입력해 주세요")
+    private String password;
+
+    @Getter
+    @Setter
+    public static class UserModifyRequestDto {
+        @NotNull(message = "정보를 입력해 주세요")
+        @NotBlank(message = "정보를 입력해 주세요")
+        private String userName;
+
+        @NotNull(message = "정보를 입력해 주세요")
+        @Past(message = "생년월일은 유효한 날짜여야 합니다.")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
+        private Date dateOfBirth;
+
+        @NotNull(message = "정보를 입력해 주세요")
+        @NotBlank(message = "정보를 입력해 주세요")
+        private String userGender;
+
+        @NotNull(message = "정보를 입력해 주세요")
+        @NotBlank(message = "정보를 입력해 주세요")
+        private String userPhone;
+
+        @NotNull(message = "정보를 입력해 주세요")
+        @NotBlank(message = "정보를 입력해 주세요")
+        private String userAddress;
+    }
+
+}
