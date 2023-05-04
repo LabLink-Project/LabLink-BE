863c9fd034d8cd079059ed8713b7e7afea052875
SoMin <thals0107@naver.com>
2023-05-04 15:15:38 +0900

    Update KakaoService.java

--- src/main/java/com/example/lablink/domain/user/kakao/service/KakaoService.java
+++ src/main/java/com/example/lablink/domain/user/kakao/service/KakaoService.java
@@ -36,8 +36,10 @@ public class KakaoService {
 
     @Transactional
     public User kakaoLogin(String code, HttpServletResponse response) throws JsonProcessingException {
+        log.info("code : " + code);
         // 1. "인가 코드"로 "액세스 토큰" 요청
         String accessToken = getToken(code);
+        log.info("accessToken : " + accessToken);
 
         // 2. 토큰으로 카카오 API 호출 : "액세스 토큰"으로 "카카오 사용자 정보" 가져오기
         KakaoUserInfoDto kakaoUserInfo = getKakaoUserInfo(accessToken);
