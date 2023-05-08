40d8a28e4fd9341c2000a20ce1f1c7fb4761cac9
rhdqors <rhdqors@gmail.com>
2023-05-08 22:42:48 +0900

    #222 feature: 유저 RefreshToken 구현
    유저 RefreshToken 기능 구현

--- src/main/java/com/example/lablink/domain/user/exception/UserErrorCode.java
+++ src/main/java/com/example/lablink/domain/user/exception/UserErrorCode.java
@@ -15,13 +15,14 @@ public enum UserErrorCode {
     PASSWORD_MISMATCH(BAD_REQUEST, "비밀번호가 일치하지 않습니다."), // 400
     DUPLICATE_PASSWORD(BAD_REQUEST, "비밀번호가 동일합니다."),
     DUPLICATE_NICK_NAME(BAD_REQUEST, "닉네임이 동일합니다."),
-//    NOT_PROPER_INPUTFORM(BAD_REQUEST, "입력한 형식이 맞지 않습니다."),
+    //    NOT_PROPER_INPUTFORM(BAD_REQUEST, "입력한 형식이 맞지 않습니다."),
 //    NOT_HAVE_PERMISSION(BAD_REQUEST, "권한이 없습니다."),
 //    NOT_AUTHOR(BAD_REQUEST, "작성자만 삭제/수정할 수 있습니다."),
 //
 //    /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     NEED_AGREE_REQUIRE_TERMS(NOT_FOUND, "필수 약관에 동의해 주세요"),
     USER_NOT_FOUND(NOT_FOUND, "등록된 사용자가 없습니다"),
+    EXPIRED_REFRESH_TOKEN(BAD_REQUEST, "쿠키에 토큰이 없습니다."),
     USERINFO_NOT_FOUND(NOT_FOUND, "등록된 사용자 정보가 없습니다"),
 //    POST_NOT_FOUND(NOT_FOUND, "선택한 게시물을 찾을 수 없습니다."),
 //    COMMENT_NOT_FOUND(NOT_FOUND, "선택한 댓글을 찾을 수 없습니다.")
