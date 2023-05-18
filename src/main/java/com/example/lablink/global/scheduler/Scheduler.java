6d9835f76d9afcc42fda9256e9c86735c2c17b26
SoMin <thals0107@naver.com>
2023-05-18 12:06:35 +0900

    #251 feat : 스케줄러를 이용해서 redis의 데이터 주기적으로 rds에 저장

--- src/main/java/com/example/lablink/global/scheduler/Scheduler.java
+++ src/main/java/com/example/lablink/global/scheduler/Scheduler.java
@@ -1,14 +1,20 @@
 package com.example.lablink.global.scheduler;
 
+import com.example.lablink.domain.study.dto.responseDto.SearchRankResponseDto;
+import com.example.lablink.domain.study.entity.SearchRankKeyword;
 import com.example.lablink.domain.study.entity.Study;
 import com.example.lablink.domain.study.entity.StudyStatusEnum;
+import com.example.lablink.domain.study.repository.SearchRankKeywordRepository;
 import com.example.lablink.domain.study.repository.StudyRepository;
+
+import com.example.lablink.domain.study.service.StudySearchService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -16,8 +22,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class Scheduler {
     private final StudyRepository studyRepository;
+    private final StudySearchService studySearchService;
+    private final SearchRankKeywordRepository searchRankKeywordRepository;
+//    private final StudyMapper studyMapper;
     /*private final JavaMailSender emailSender;
-    private final FeedBackService feedBackService;*/
+        private final FeedBackService feedBackService;*/
     // 초, 분, 시, 일, 월, 주 순서
     @Scheduled(cron = "0 0 0 * * *")
     // todo : exception ?
@@ -34,38 +43,20 @@ public class Scheduler {
             }
         }
     }
-    /*@Scheduled(fixedDelay = 500000)
-    public void emailExcelSend(){
-       List<Study> studies = studyRepository.findAllByEmailSendIsFalse();
-        for (Study study : studies) {
-            // endDate와 localDate 비교 및 studystatus 변경
-            LocalDateTime currentDateTime = LocalDateTime.now();
-            // endDate가 localDate보다 이전인 경우
-            if (study.getEndDate().isBefore(currentDateTime)) {
-                MimeMessage mimeMessage = emailSender.createMimeMessage();
 
-                try {
-                    MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, true, "UTF-8"); // use multipart (true)
-
-                    mimeMessageHelper.setSubject(MimeUtility.encodeText("피드백 내용 메일로 전달 드립니다", "UTF-8", "B")); // Base64 encoding
-                    mimeMessageHelper.setText("피드백 내용 메일로 전달 드립니다.");
-                    mimeMessageHelper.setFrom(new InternetAddress("fghij7410@gmai.com"));
-                    mimeMessageHelper.setTo(study.getCompany().getEmail());
-                    *//*String filePath = System.getProperty("user.home") + File.separator + "Downloads" + File.separator + "FeedBack.xlsx";
-                    String filePath = System.setProperty(feedBackService.studentExcelDownloadXSSF(study.getId()),true);
-                    FileSystemResource fileSystemResource = new FileSystemResource(new File(filePath));*//*
-                    XSSFWorkbook fileOutputStream = feedBackService.studentExcelDownloadXSSF(study.getId());
-                    String filePath = fileOutputStream.getCTWorkbook().xmlText();
-                    System.out.println(filePath);
-                    mimeMessageHelper.addAttachment(MimeUtility.encodeText("피드백 내용.xlsx","UTF-8", "B"),new File(filePath)));
-
-                    emailSender.send(mimeMessage);
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
+    // 매일 자정 인기 검색어 rds로 저장
+    @Scheduled(cron = "0 0 0 * * *")
+    public void saveKeyword() {
+        log.info("매일 자정 인기 검색어 rds로 저장");
+        List<SearchRankResponseDto> searchRanks = studySearchService.searchRankList();
+        // searchrank entity 만들어서 저장
+        List<SearchRankKeyword> searchs = new ArrayList<>();
+        for (SearchRankResponseDto searchRank : searchRanks) {
+            SearchRankKeyword search = new SearchRankKeyword();
+            search.setRankKeyword(searchRank.getRankKeyword());
+            search.setScore(searchRank.getScore());
+            searchs.add(search);
         }
-
-    }*/
+        searchRankKeywordRepository.saveAll(searchs);
+    }
 }
