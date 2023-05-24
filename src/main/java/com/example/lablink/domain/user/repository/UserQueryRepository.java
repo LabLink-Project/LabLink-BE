bcad915dc60a8023a8fae5691458990805682a1f
rhdqors <>
2023-05-24 21:38:38 +0900

    #260 refactoring: getmylabs 캐싱 처리
    1. 인덱싱 및 캐싱처리 -> 응답속도 57%에서 80%로 개선

--- src/main/java/com/example/lablink/domain/user/repository/UserQueryRepository.java
+++ src/main/java/com/example/lablink/domain/user/repository/UserQueryRepository.java
@@ -1,7 +1,6 @@
 package com.example.lablink.domain.user.repository;
 
 import com.example.lablink.domain.user.dto.response.MyLabResponseDto;
-import com.example.lablink.domain.user.entity.QUser;
 import com.example.lablink.domain.application.entity.QApplication;
 import com.example.lablink.domain.study.entity.QStudy;
 import com.example.lablink.domain.user.entity.User;
@@ -12,11 +11,9 @@ import org.springframework.stereotype.Repository;
 import javax.persistence.EntityManager;
 import java.util.List;
 
-// 내 신청서 목록 조회 querydsl을 위한 클래스 ( 비교 결과 jpql과 큰 차이는 없다 )
 @Repository
 public class UserQueryRepository {
     private final JPAQueryFactory queryFactory;
-    private final QUser qUser = QUser.user;
     private final QStudy qStudy = QStudy.study;
     private final QApplication qApplication = QApplication.application;
 
@@ -26,7 +23,17 @@ public class UserQueryRepository {
 
     public List<MyLabResponseDto> getMyLabResponseDto(User userDetails) {
         return queryFactory
-                .select(Projections.constructor(MyLabResponseDto.class, qStudy, qApplication.applicationViewStatusEnum, qApplication.approvalStatusEnum))
+                .select(Projections.constructor(
+                        MyLabResponseDto.class,
+                        qStudy.id,
+                        qStudy.title,
+                        qStudy.createdAt,
+                        qStudy.pay,
+                        qStudy.address,
+                        qApplication.applicationViewStatusEnum,
+                        qApplication.approvalStatusEnum,
+                        qStudy.date,
+                        qStudy.company.companyName))
                 .from(qStudy)
                 .leftJoin(qApplication)
                 .on(qStudy.id.eq(qApplication.studyId))
